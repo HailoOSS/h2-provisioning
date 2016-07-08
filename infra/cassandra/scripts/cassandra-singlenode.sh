9c14f551af0384a7cfc7fc9f36226374adc8b27e
Josh Holloway <josh.holloway@hailocab.com>
2016-07-08 11:35:00 +0100

    Add BOXEN_RABBITMQ_URL for kernel services

--- infra/cassandra/scripts/cassandra-singlenode.sh
+++ infra/cassandra/scripts/cassandra-singlenode.sh
@@ -2,7 +2,7 @@
 
 # Disable virtual nodes
 sed -i -e "s/num_tokens/\#num_tokens/" $CASSANDRA_CONFIG/cassandra.yaml
-
+sed -i -e "s|commitlog_directory=.*|commitlog_directory=/tmp" $CASSANDRA_CONFIG/cassandra.yaml
 # Pointless in one-node cluster, saves about 5 sec waiting time
 echo "JVM_OPTS=\"\$JVM_OPTS -Dcassandra.skip_wait_for_gossip_to_settle=0\"" >> $CASSANDRA_CONFIG/cassandra-env.sh
 
